c5fa0785d6921df7ea2b3eb7d9a96b3863f1a8a3
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-08-05 13:09:48 +0400

    implement function utils in java and streams

---
+++ src/main/java/streams/StreamOperations.java
@@ -0,0 +1,83 @@
+package streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamOperations {
+
+    static List<String> veggies = List.of(
+            "spinach",
+            "cabbage",
+            "peas",
+            "green beans",
+            "brussel sprouts",
+            "carrots"
+    );
+
+    public static void main(String[] args) {
+        anyMatchDemo();
+        allMatchDemo();
+        filterDemo();
+        mapDemo();
+        reduceDemo();
+        intReduceDemo();
+        collectDemo();
+        multiOperationDemo();
+    }
+
+    public static void anyMatchDemo() {
+        boolean anyMatches = veggies.stream().anyMatch(v -> v.contains(" "));
+
+        System.out.println(anyMatches);
+    }
+
+    public static void allMatchDemo() {
+        boolean allMatches = veggies.stream().allMatch(v -> v.contains("s"));
+        System.out.println(allMatches);
+    }
+
+    public static void filterDemo() {
+        veggies.stream()
+                .filter(v -> v.startsWith("c"))
+                .forEach(System.out::println);
+    }
+
+    public static void mapDemo() {
+        veggies.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
+    }
+
+    public static void reduceDemo() {
+        String result = veggies.stream()
+                .sorted()
+                .reduce("", (a, b) -> a + " | " + b);
+
+        System.out.println(result);
+    }
+
+    public static void intReduceDemo() {
+        List<Integer> numbers = List.of(2,4,6,8);
+        Integer totalAmount = numbers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(totalAmount);
+    }
+
+    public static void collectDemo() {
+        List<String> veggiesEndingInS = veggies.stream().filter(v -> v.endsWith("s"))
+                .collect(Collectors.toList());
+
+        System.out.println(veggiesEndingInS);
+        System.out.println(veggies);
+        veggiesEndingInS.forEach(System.out::println);
+    }
+
+    public static void multiOperationDemo() {
+        veggies.stream()
+                .sorted()
+                .filter(v -> v.startsWith("c"))
+                .map(String::toUpperCase)
+                .map(v -> v.transform(w -> "yummy " + w))
+                .forEach(System.out::println);
+    }
+
+}
